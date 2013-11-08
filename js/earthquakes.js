4825ff351f794d3bc572215da266726c68f13a45
cpf5193 <cpf5193@cs.washington.edu>
2013-11-07 18:18:21 -0800

    Complete all features except filter and refresh

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -30,3 +30,58 @@ $(document).ajaxError(function(event, jqXHR, err){
     alert('Problem obtaining data: ' + jqXHR.statusText);
 });
 
+
+//function to call when document is ready
+$(function(){
+    getQuakes();
+}); //doc ready
+
+
+/*Queries the server to get the quake data and sets up the page*/
+function getQuakes(){
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+		gov.usgs.quakes = quakes;
+		$('.loading').removeClass('loading');
+		$('.message').html('Displaying ' + quakes.length + ' earthquakes');
+		gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+			center: new google.maps.LatLng(0,0),
+			zoom: 2,
+			mapTypeId: google.maps.MapTypeId.TERRAIN,
+			streetViewControl: false
+		});
+		addQuakeMarkers(quakes, gov.usgs.quakesMap);
+	});
+}
+
+/*
+Adds the markers to the map
+quakes: array of quake objects
+map: google map which will hold markers
+*/
+function addQuakeMarkers(quakes, map){
+	var quake, i, loc;
+	for(i=0; i<quakes.length; i++){
+		quake = quakes[i];
+		loc = quake.location;
+		if(loc){
+			quake.mapMarker = new google.maps.Marker({
+				map: map,
+				position: new google.maps.LatLng(loc.latitude, loc.longitude),
+				datetime: quake.datetime,
+				magnitude: quake.magnitude,
+				depth: quake.depth
+			});
+			google.maps.event.addListener(quake.mapMarker, 'click', function(){
+				if(gov.usgs.iw){
+					gov.usgs.iw.close();
+				}
+				gov.usgs.iw = new google.maps.InfoWindow({
+				content: new Date(this.datetime).toLocaleString() + 
+					': magnitude ' + this.magnitude + ' at a depth of '
+					+ this.depth + ' meters' 
+				});
+				gov.usgs.iw.open(map, this);
+			});
+		}
+	}
+}
